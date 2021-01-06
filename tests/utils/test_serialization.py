5cb6bac0961bbe16441fadbd1ceca33664a8c2d5
danielenricocahall <danielenricocahall@gmail.com>
2021-01-05 21:05:45 -0500

    fix tests

--- tests/utils/test_serialization.py
+++ tests/utils/test_serialization.py
@@ -1,16 +1,19 @@
 import pytest
+from tensorflow.keras.layers import Dense
 from tensorflow.keras.models import Sequential
 from elephas.utils import serialization
 
 
 def test_model_to_dict():
     model = Sequential()
+    model.add(Dense(1, input_shape=(1,)))
     dict_model = serialization.model_to_dict(model)
     assert list(dict_model.keys()) == ['model', 'weights']
 
 
 def test_dict_to_model():
     model = Sequential()
+    model.add(Dense(1, input_shape=(1,)))
     dict_model = serialization.model_to_dict(model)
 
     recovered = serialization.dict_to_model(dict_model)
