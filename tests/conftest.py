50666a0c9ef81121fc412189874ff5eda71f319b
daniel.cahall <danielenricocahall@gmail.com>
2025-08-05 21:38:58 -0400

    feature: reduce size of classification model used for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,39 +8,39 @@ from tensorflow.keras.utils import to_categorical
 @pytest.fixture
 def classification_model():
     model = Sequential()
-    model.add(Dense(128, input_dim=784))
-    model.add(Activation('relu'))
+    model.add(Dense(32, input_dim=784))
+    model.add(Activation("relu"))
     model.add(Dropout(0.2))
-    model.add(Dense(128))
-    model.add(Activation('relu'))
+    model.add(Dense(16))
+    model.add(Activation("relu"))
     model.add(Dropout(0.2))
     model.add(Dense(10))
-    model.add(Activation('softmax'))
+    model.add(Activation("softmax"))
     return model
 
 
 @pytest.fixture
 def regression_model():
     model = Sequential()
-    model.add(Dense(64, activation='relu', input_shape=(13,)))
-    model.add(Dense(64, activation='relu'))
-    model.add(Dense(1, activation='linear'))
+    model.add(Dense(64, activation="relu", input_shape=(13,)))
+    model.add(Dense(64, activation="relu"))
+    model.add(Dense(1, activation="linear"))
     return model
 
 
 @pytest.fixture
 def classification_model_functional():
-    input_layer = Input(shape=(784, ))
-    hidden = Dense(128, activation='relu')(input_layer)
+    input_layer = Input(shape=(784,))
+    hidden = Dense(128, activation="relu")(input_layer)
     dropout = Dropout(0.2)(hidden)
-    hidden2 = Dense(128, activation='relu')(dropout)
+    hidden2 = Dense(128, activation="relu")(dropout)
     dropout2 = Dropout(0.2)(hidden2)
-    output = Dense(10, activation='softmax')(dropout2)
+    output = Dense(10, activation="softmax")(dropout2)
     model = Model(inputs=input_layer, outputs=output)
     return model
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def mnist_data():
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
@@ -57,8 +57,7 @@ def mnist_data():
     return x_train, y_train, x_test, y_test
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def boston_housing_dataset():
     (x_train, y_train), (x_test, y_test) = boston_housing.load_data()
     return x_train, y_train, x_test, y_test
-
